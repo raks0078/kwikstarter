e77d63dd85c8e6a07bcb4d4cf8d61fc2b599d266
Abhishek <sharmaabhishek098@gmail.com>
2021-03-12 00:15:26 +0530

    updates

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./Header.scss"
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Header
