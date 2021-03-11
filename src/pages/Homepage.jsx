7bfa195ff784fd57c55131d25e5b91e628eb15cf
Abhishek <sharmaabhishek098@gmail.com>
2021-03-12 00:19:14 +0530

    updates

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./Homepage.scss"
+class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Homepage
